54f98a1dfe5cc2b45b6aab8f158a580df8748d9f
gcabelo <gustavo.s.cabelo@gmail.com>
2025-05-12 00:08:50 -0300

    [gsc][feat] Adjusted the run() and train() to get the 'company_name' as input parameter and create a crew_instance to pass the inputs at the build_crew method.

--- src/ai_poc_crew_ai/main.py
+++ src/ai_poc_crew_ai/main.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python
+import os
 import sys
 import warnings
-
+import argparse
 from datetime import datetime
 
-from ai_poc_crew_ai.crew import AiPocCrewAi
+from crew import AiPocCrewAi
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# Create output directory if it doesn't exist
+os.makedirs('output', exist_ok=True)
 
 # This main file is intended to be a way for you to run your
 # crew locally, so refrain from adding unnecessary logic into this file.
 # Replace with inputs you want to test with, it will automatically
 # interpolate any tasks and agents information
 
-def run():
+def run(company_name: str):
     """
     Run the crew.
     """
     inputs = {
-        'company_name': {company_name},
-        'current_year': str(datetime.now().year)
+        'company_name': company_name,
+        'current_year': str(datetime.now().year),
+        'current_date': str(datetime.now().strftime("%Y%m%d"))
     }
     
     try:
-        AiPocCrewAi().crew().kickoff(inputs=inputs)
+        crew_instance = AiPocCrewAi().build_crew(inputs)
+        crew_instance.kickoff(inputs=inputs)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
 
-def train():
+def train(company_name: str):
     """
     Train the crew for a given number of iterations.
     """
     inputs = {
-        "topic": "AI LLMs",
-        'current_year': str(datetime.now().year)
+        'company_name': company_name,
+        'current_year': str(datetime.now().year),
+        'current_date': str(datetime.now().strftime("%Y%m%d"))
     }
     try:
         AiPocCrewAi().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
@@ -57,7 +64,7 @@ def test():
     Test the crew execution and returns the results.
     """
     inputs = {
-        "topic": "AI LLMs",
+        'company_name': company_name,
         "current_year": str(datetime.now().year)
     }
     
@@ -66,3 +73,10 @@ def test():
 
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run CrewAI workflow for a given company.")
+    parser.add_argument("--company_name", required=True, help="Name of the company to analyze")
+
+    args = parser.parse_args()
+    run(args.company_name)
